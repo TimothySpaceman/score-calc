4d75c7cbebf7f5f9b4ad1b6071468b369a8cffcf
Timothy Rehenel <timothy.rehenel@gmail.com>
2025-12-10 11:42:05 +0200

    feat: method for reading multiple values

--- src/terminal/Input.java
+++ src/terminal/Input.java
@@ -1,5 +1,7 @@
 package terminal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -11,7 +13,20 @@ public class Input {
         return scanner.nextLine();
     }
 
-    public <T> T getValue(String message, Function<String, T> parser, Predicate<T> validator) {
+    public <T> T getValue(
+            String message,
+            Function<String, T> parser,
+            Predicate<T> validator
+    ) {
+        return getValue(message, parser, validator, "Please, input valid data");
+    }
+
+    public <T> T getValue(
+            String message,
+            Function<String, T> parser,
+            Predicate<T> validator,
+            String errorMessage
+    ) {
         if (message != null && !message.isEmpty()) {
             System.out.println(message);
         }
@@ -23,11 +38,54 @@ public class Input {
             value = parser.apply(readLine());
             isValid = validator.test(value);
             if (!isValid) {
-                System.out.println("Please, input valid data");
+                System.out.println(errorMessage);
             }
         } while (!isValid);
 
         System.out.println();
         return value;
     }
+
+    public <T> List<T> getValues(
+            String message,
+            Function<String, T> parser,
+            Predicate<T> validator
+    ) {
+        return getValues(message, parser, validator, "Please, input valid data");
+    }
+
+    public <T> List<T> getValues(
+            String message,
+            Function<String, T> parser,
+            Predicate<T> validator,
+            String errorMessage
+    ) {
+        if (message != null && !message.isEmpty()) {
+            System.out.println(message);
+        }
+
+        List<T> values = new ArrayList<>();
+
+        while (true) {
+            System.out.print("> ");
+            String input = readLine();
+
+            T value = parser.apply(input);
+
+            if (value == null) {
+                System.out.println();
+                break;
+            }
+
+            if (!validator.test(value)) {
+                System.out.println(errorMessage);
+                continue;
+            }
+
+            values.add(value);
+        }
+
+        return values;
+    }
+
 }
